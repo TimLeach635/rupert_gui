8b63da6745f8d1f7d100cff7f9a3848aef301b0c
Tim Leach <4410524+TimLeach635@users.noreply.github.com>
2025-12-28 14:29:32 +0000

    chore: rename from template

--- assets/sw.js
+++ assets/sw.js
@@ -1,9 +1,9 @@
-var cacheName = 'egui-template-pwa';
+var cacheName = 'rupert-gui-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  './rupert_gui.js',
+  './rupert_gui_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
