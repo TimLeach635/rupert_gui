3aa2c1bf0e8d46a1f04f2374371a162d3f2074b6
TimLeach635 <4410524+TimLeach635@users.noreply.github.com>
2025-12-29 22:36:49 +0000

    Deploying to gh-pages from @ TimLeach635/rupert_gui@c20c39ebd0d1b27f85783b6abcdd53e4f4d2a9ec 🚀

---
+++ sw.js
@@ -0,0 +1,25 @@
+var cacheName = 'rupert-gui-pwa';
+var filesToCache = [
+  './',
+  './index.html',
+  './rupert_gui.js',
+  './rupert_gui_bg.wasm',
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
